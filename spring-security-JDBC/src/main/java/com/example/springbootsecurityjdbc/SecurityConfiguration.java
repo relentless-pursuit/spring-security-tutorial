c46c892f2615f5398a82275e27f06f7de5eb85f2
piyush kumar sadangi <piyushkumarsadangi@piyushs-MacBook-Air.local>
2023-03-05 16:30:06 +0530

    tutorial to show spring-security using JDBC connection

---
+++ spring-security-JDBC/src/main/java/com/example/springbootsecurityjdbc/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.springbootsecurityjdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Autowired
+    public void authManager(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .usersByUsernameQuery("select username,password,enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username,authority from authorities where username=?");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((req) -> req
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                .anyRequest().authenticated()
+        ).formLogin();
+        return  http.build();
+    }
+
+}
